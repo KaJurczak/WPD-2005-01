6b99d38e4ad8315ed9df30b8099b0a34b92b3c91
Dawid Krystochowicz <daw_kryst@interia.pl>
2020-05-20 22:18:32 +0200

    add set viewport method

--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -4,16 +4,46 @@ import PropTypes from 'prop-types';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 
-const MainLayout = ({ children }) => (
-  <div>
-    <Header />
-    {children}
-    <Footer />
-  </div>
-);
+class MainLayout extends React.Component {
+  initViewport() {
+    const { viewport, setViewport } = this.props;
+    const viewportWidth = Number(window.innerWidth);
+    let newViewport;
+    if (viewportWidth >= 992) {
+      newViewport = 'desktop';
+    } else if (viewportWidth >= 768) {
+      newViewport = 'tablet';
+    } else {
+      newViewport = 'mobile';
+    }
+    if (newViewport !== viewport) setViewport(newViewport);
+  }
+
+  componentDidMount() {
+    this.initViewport();
+    window.addEventListener('resize', this.initViewport.bind(this));
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.initViewport.bind(this));
+  }
+
+  render() {
+    const { children } = this.props;
+    return (
+      <div>
+        <Header />
+        {children}
+        <Footer />
+      </div>
+    );
+  }
+}
 
 MainLayout.propTypes = {
   children: PropTypes.node,
+  viewport: PropTypes.string,
+  setViewport: PropTypes.func,
 };
 
 export default MainLayout;
