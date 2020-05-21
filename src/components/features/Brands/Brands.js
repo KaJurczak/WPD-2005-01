3675ea10fee6c90c6b77120529731b9b97aee321
Maciej Smolarczyk <smolarczyk.maciek@gmail.com>
2020-05-21 11:17:00 +0200

    Change Brands.js

--- src/components/features/Brands/Brands.js
+++ src/components/features/Brands/Brands.js
@@ -5,43 +5,41 @@ import Button from '../../common/Button/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
-const Brands = () => {
-  return (
-    <div className={styles.root}>
-      <div className='container'>
-        <div className={`row align-items-center ${styles.wrapper}`}>
-          <Button className={styles.button}>
-            <FontAwesomeIcon icon={faAngleLeft} className={styles.icon} />
-          </Button>
+const brandsData = [
+  'https://pics.tinypic.pl/i/01006/u3zvh8bu88b4.png',
+  'https://pics.tinypic.pl/i/01006/u3zvh8bu88b4.png',
+  'https://pics.tinypic.pl/i/01006/u3zvh8bu88b4.png',
+  'https://pics.tinypic.pl/i/01006/u3zvh8bu88b4.png',
+  'https://pics.tinypic.pl/i/01006/u3zvh8bu88b4.png',
+  'https://pics.tinypic.pl/i/01006/u3zvh8bu88b4.png',
+];
 
-          <div className={`row col ${styles.brands}`}>
-            <div className={`col ${styles.image}`}>
-              <img src='https://pics.tinypic.pl/i/01006/u3zvh8bu88b4.png' alt=''></img>
+class Brands extends React.Component {
+  render() {
+    const brands = brandsData;
+
+    return (
+      <div className={styles.root}>
+        <div className='container'>
+          <div className={`row align-items-center ${styles.wrapper}`}>
+            <Button className={styles.button}>
+              <FontAwesomeIcon icon={faAngleLeft} className={styles.icon} />
+            </Button>
+            <div className={`row col ${styles.brands}`}>
+              {brands.map(item => (
+                <div key={item.id} className={`col ${styles.image}`}>
+                  <img src={item} alt=''></img>
+                </div>
+              ))}
             </div>
-            <div className={`col ${styles.image}`}>
-              <img src='https://pics.tinypic.pl/i/01006/u3zvh8bu88b4.png' alt=''></img>
-            </div>
-            <div className={`col ${styles.image}`}>
-              <img src='https://pics.tinypic.pl/i/01006/u3zvh8bu88b4.png' alt=''></img>
-            </div>
-            <div className={`col ${styles.image}`}>
-              <img src='https://pics.tinypic.pl/i/01006/u3zvh8bu88b4.png' alt=''></img>
-            </div>
-            <div className={`col ${styles.image}`}>
-              <img src='https://pics.tinypic.pl/i/01006/u3zvh8bu88b4.png' alt=''></img>
-            </div>
-            <div className={`col ${styles.image}`}>
-              <img src='https://pics.tinypic.pl/i/01006/u3zvh8bu88b4.png' alt=''></img>
-            </div>
+            <Button className={styles.button}>
+              <FontAwesomeIcon icon={faAngleRight} className={styles.icon} />
+            </Button>
           </div>
-
-          <Button className={styles.button}>
-            <FontAwesomeIcon icon={faAngleRight} className={styles.icon} />
-          </Button>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Brands;
