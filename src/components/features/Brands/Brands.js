6a65bad0f398ae713cf3e61e666be2e3f703aad6
Maciej Smolarczyk <smolarczyk.maciek@gmail.com>
2020-05-23 17:54:06 +0200

    Add logo and delete unused class after CR

--- src/components/features/Brands/Brands.js
+++ src/components/features/Brands/Brands.js
@@ -7,11 +7,11 @@ import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 const brandsData = [
   'https://pics.tinypic.pl/i/01006/u3zvh8bu88b4.png',
-  'https://pics.tinypic.pl/i/01006/u3zvh8bu88b4.png',
-  'https://pics.tinypic.pl/i/01006/u3zvh8bu88b4.png',
-  'https://pics.tinypic.pl/i/01006/u3zvh8bu88b4.png',
-  'https://pics.tinypic.pl/i/01006/u3zvh8bu88b4.png',
-  'https://pics.tinypic.pl/i/01006/u3zvh8bu88b4.png',
+  'https://images.tinypic.pl/i/01006/lo0wdsvhnaa7.jpg',
+  'https://images.tinypic.pl/i/01006/nc6o24wtfgbr.png',
+  'https://images.tinypic.pl/i/01006/ngsy1aua7sjf.png',
+  'https://images.tinypic.pl/i/01006/13stvoi1fsei.png',
+  'https://images.tinypic.pl/i/01006/xsxob3cvjje1.gif',
 ];
 
 class Brands extends React.Component {
@@ -25,7 +25,7 @@ class Brands extends React.Component {
             <Button className={styles.button}>
               <FontAwesomeIcon icon={faAngleLeft} className={styles.icon} />
             </Button>
-            <div className={`row col ${styles.brands}`}>
+            <div className={`row col`}>
               {brands.map(item => (
                 <div key={item.id} className={`col ${styles.image}`}>
                   <img src={item} alt=''></img>
