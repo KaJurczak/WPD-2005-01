c2ef5f9b9c4c45c9ae110870bdb4bbbc1bf84a78
Maciej Smolarczyk <smolarczyk.maciek@gmail.com>
2020-05-24 12:44:15 +0200

    Add baner to Product.js

--- src/components/views/Products/Products.js
+++ src/components/views/Products/Products.js
@@ -2,8 +2,17 @@ import React from 'react';
 import styles from './Products.module.scss';
 
 const Products = () => (
-  <div>
-    <h1 className={styles.title}>Products</h1>
+  <div className={styles.root}>
+    <div className={'container'}>
+      <div className={styles.baner}>
+        <h1 className={styles.banerTitle}>
+          BEDROOM <b>FURNITURE</b>
+        </h1>
+        <h3 className={styles.banerSubtitle}>
+          &nbsp;ALWAYS <a> 25% </a> OFF OR MORE
+        </h3>
+      </div>
+    </div>
   </div>
 );
 
