1ddd261fe8709a139c176a08676f6239e12659f7
Maciej Smolarczyk <smolarczyk.maciek@gmail.com>
2020-05-25 20:27:20 +0200

    Add padding to column and pageMenu in Products.js after CR

--- src/components/views/Products/Products.js
+++ src/components/views/Products/Products.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from './Products.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 const Products = () => (
   <div className={styles.root}>
@@ -12,6 +14,17 @@ const Products = () => (
           &nbsp;ALWAYS <a> 25% </a> OFF OR MORE
         </h3>
       </div>
+
+      <div className={styles.pageMenu}>
+        <a href='#'>
+          Home
+          <FontAwesomeIcon className={styles.icon} icon={faAngleRight} />
+        </a>
+        <a href='#' className={styles.pageActive}>
+          Furniture
+        </a>
+      </div>
+
       <div className={`row ${styles.columnWrapper}`}>
         <div className={`col-12 col-sm-9 ${styles.columnFurniture}`}>
           TEST COLUMN FURNITURE TEST COLUMN FURNITURE TEST COLUMN FURNITURE TEST COLUMN
